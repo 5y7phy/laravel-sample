c12af4148aea42d8a1a0ce4997e5d5cbed1ccc88
yuga <uoma.libero@icloud.com>
2025-11-12 13:37:04 +0900

    Add migration for posts table

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,10 +1,29 @@
 <?php
-
+ 
 namespace App\Models;
-
+ 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+ 
 class Post extends Model
 {
-    //
+    use HasFactory;
+ 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'contents',
+    ];
+ 
+    /**
+     * Disable timestamps (created_at, updated_at)
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 }
